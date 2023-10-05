49bb89b47487432390e508fc16edd86beb7172fc
theauv <56826362+theauv@users.noreply.github.com>
2023-10-05 16:50:28 +0200

    configs for wandb and cleaned scripts

---
+++ run_scripts/random_agent.py
@@ -0,0 +1,59 @@
+import matplotlib
+from matplotlib import pyplot as plt
+import torch
+import numpy as np
+import omegaconf
+import hydra
+
+
+from src.env.env_handler import EnvironmentHandler
+
+
+@hydra.main(config_path="configs", config_name="main")
+def run(cfg: omegaconf.DictConfig):
+
+    #create env and random seed
+    env, term_fn, reward_fn = EnvironmentHandler.make_env(cfg)
+    np.random.seed(cfg.seed)
+    torch.manual_seed(cfg.seed)
+
+
+    observation, info = env.reset(seed=42)
+
+    all_n_steps = []
+    all_rewards = []
+
+    n_steps = 0
+    rewards = 0
+    for _ in range(1000):
+        action = env.action_space.sample()  # this is where you would insert your policy
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        n_steps += 1
+        rewards += reward
+
+        if terminated or truncated:
+            print("Yes")
+            all_n_steps.append(n_steps)
+            all_rewards.append(rewards)
+            observation, info = env.reset()
+            n_steps = 0
+            rewards = 0
+    env.close()
+
+    return all_n_steps, all_rewards
+
+
+if __name__ == "__main__":
+
+    all_n_steps, all_rewards = run()
+
+    matplotlib.use("TkAgg")
+    fig, axs = plt.subplots(1, 2)
+    axs[0].plot(all_n_steps)
+    axs[1].plot(all_rewards)
+    axs[0].set_xlabel("Episode")
+    axs[1].set_xlabel("Episode")
+    axs[0].set_ylabel("Number of steps")
+    axs[1].set_ylabel("Total reward")
+    plt.show()
---
+++ run_scripts/train.py
@@ -0,0 +1,42 @@
+import wandb
+import torch
+import omegaconf
+import hydra
+import numpy as np
+
+import mbrl.algorithms.mbpo as mbpo
+import mbrl.algorithms.pets as pets
+import mbrl.algorithms.planet as planet
+
+import src.algorithm.pets_adapted as pets_adatpted
+from src.env.env_handler import EnvironmentHandler
+from src.util.util import get_run_kwargs
+
+
+@hydra.main(config_path="configs", config_name="main")
+def run(cfg: omegaconf.DictConfig):
+    #set-up run and api
+    if cfg.experiment.api_name == "wandb":
+        # start a new wandb run to track this script
+        wandb.init(**get_run_kwargs(cfg))
+
+    #create env and random seed
+    env, term_fn, reward_fn = EnvironmentHandler.make_env(cfg)
+    np.random.seed(cfg.seed)
+    torch.manual_seed(cfg.seed)
+
+    #train the agent and model with the given algorithm
+    if cfg.algorithm.name == "pets_adapted":
+        return pets_adatpted.train(env, term_fn, reward_fn, cfg)
+    if cfg.algorithm.name == "pets":
+        return pets.train(env, term_fn, reward_fn, cfg)
+    if cfg.algorithm.name == "mbpo":
+        test_env, *_ = EnvironmentHandler.make_env(cfg)
+        return mbpo.train(env, test_env, term_fn, cfg)
+    if cfg.algorithm.name == "planet":
+        return planet.train(env, cfg)
+    wandb.finish()
+
+
+if __name__ == "__main__":
+    run()
