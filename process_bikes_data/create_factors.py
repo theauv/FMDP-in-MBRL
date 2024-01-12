8286fe9c6e9fa7811770ab61161563af25f1e816
theauv <56826362+theauv@users.noreply.github.com>
2024-01-12 15:53:00 +0100

    managing

---
+++ process_bikes_data/create_factors.py
@@ -0,0 +1,90 @@
+"""
+This file contains functions that create an adjacency matrix based on certain 
+criteria. Therefore obtaining factors that can be induced in the Bikes environment 
+"""
+
+import argparse
+import os
+
+from matplotlib import pyplot as plt
+import networkx as nx
+import numpy as np
+from pyvis.network import Network
+from scipy.spatial.distance import cdist
+import geopy.distance
+
+
+def create_local_factor(centroid_coords, radius):
+
+    num_centroids = len(centroid_coords)
+    adjacency = np.zeros((num_centroids, num_centroids))
+
+    for centroid_i, coords_i in enumerate(centroid_coords):
+        for centroid_j, coords_j in enumerate(centroid_coords):
+            distance = geopy.distance.distance(coords_i, coords_j).km
+            if distance <= radius:
+                adjacency[centroid_i, centroid_j] = 1
+
+    return adjacency
+
+
+def draw_map_factors(centroid_coords, adjacency):
+    # TODO: draw transparent radius around nodes to show the actual radius?
+
+    graph_size = 600
+    net = Network(f"{graph_size}px", select_menu=True)
+    net.toggle_physics(False)
+
+    for i, centroid_coord in enumerate(centroid_coords):
+        centroid_coord = np.flip(centroid_coord)
+        net.add_node(
+            i,
+            label=i,
+            x=centroid_coord[0] * graph_size ** 2,
+            y=(graph_size - centroid_coord[1]) * graph_size ** 2,
+            color="blue",
+            size=100,
+        )
+    for centroid_i, factor_i in enumerate(adjacency):
+        for centroid_j, scope_j in enumerate(factor_i):
+            if scope_j > 0.0 and centroid_i != centroid_j:
+                net.add_edge(centroid_i, centroid_j, width=0.1)
+
+    for n in net.nodes:
+        n["font"] = {"size": 1000}
+
+    net.show("adjacency_factor_vis.html", notebook=False)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--centroid_coords_file",
+        type=str,
+        default="src/env/bikes_data/LouVelo_centroids_coords.npy",
+        help="File containing the coordinates of the centroids",
+    )
+    parser.add_argument(
+        "--radius",
+        type=float,
+        default=3,
+        help="Maximum trip distance starting from any centroid (in km)",
+    )
+    parser.add_argument(
+        "--test",
+        action="store_true",
+        help="Use it if you just want to test the sparsity without storing the adjacency",
+    )
+    args = parser.parse_args()
+
+    centroid_coords = np.load(args.centroid_coords_file)
+    adjacency = create_local_factor(centroid_coords, args.radius)
+    filename = f"factors_radius_{int(args.radius)}"
+    directory = os.path.dirname(os.path.relpath(__file__))
+    file_path = os.path.join(directory, filename)
+    print("Adjacency", adjacency)
+    ratio = round(np.count_nonzero(adjacency) / (len(adjacency) ** 2), 3) * 100
+    print(f"Sparsity: {ratio}% of non zeros")
+    draw_map_factors(centroid_coords, adjacency)
+    if not args.test:
+        np.save(file_path, adjacency)
---
+++ src/agent/heuristic.py
@@ -0,0 +1,163 @@
+from typing import Optional, Union, List
+from numbers import Number
+import gymnasium as gym
+import numpy as np
+from scipy.spatial import distance
+import geopy.distance
+
+from mbrl.planning.core import Agent
+
+from src.env.bikes import Bikes
+
+
+class StubbornAgent(Agent):
+    def __init__(
+        self, env: gym.Env, action: Optional[Union[Number, List[Number]]] = None
+    ) -> None:
+        """
+        Agent repeating the exact same action each environment step.
+        :param action: The action can be directly pass to the agent 
+        (needs to be of the right dtype and dimension).
+        Or you can pass a single value, then the whole action will be
+        equal to this value for each action entry
+        Or by default, None will sample a random action and fix it for the whole
+        simulation. 
+        """
+        if action is None:
+            self.action = env.action_space.sample()
+        elif hasattr(action, "__len__"):
+            self.action = action
+        else:
+            # TODO: Maybe add "garde-fou", but should already exist in gym.Env
+            self.action = np.ones(env.action_space.shape) * action
+
+    def act(self, obs: np.ndarray, **_kwargs) -> np.ndarray:
+        """
+        Return the same initialized action regardless of the observation
+        """
+        return self.action
+
+
+class GoodBikesHeuristic(Agent):
+    """
+        TODO: Does not take into account taken_bikes
+        TODO: Greedily solve each timeshift but not the whole day
+        TODO: Does not take into account demand of a centroid being > bikes_per_truck
+        TODO: Put as much as bikes as possible each time (does not try to minimize the number
+        of bikes)
+    """
+
+    def __init__(self, env_config, env: Bikes) -> None:
+
+        self.all_trips_data = env.all_trips_data
+        self.action_timeshifts = env.action_timeshifts
+        self.centroids = env.centroid_coords
+        self.start_walk_dist_max = env_config.start_walk_dist_max
+        self.end_walk_dist_max = env_config.end_walk_dist_max
+        self.map_obs = env.map_obs
+        self.map_act = env.map_act
+        self.num_trucks = env_config.num_trucks
+        self.bikes_per_truck = env_config.bikes_per_truck
+        self.action_shape = env.action_space.shape
+
+    def get_timeshift(self, counter):
+        """
+        get the current timeshift, namely the time between 
+        2 actions (the one we take now and the next one).
+
+        :param counter: number of actions took so far. As the total
+        number of actions per day is fixed we can find the timeshift from it.
+        :return: current timeshift
+        """
+        if counter < len(self.action_timeshifts) - 1:
+            return self.action_timeshifts[counter : counter + 2]
+        elif counter >= len(self.action_timeshifts) - 1:
+            return None
+
+    def get_current_trips_data(self, month, day, time_counter):
+        """
+        Get the current trips data to forsee what will be the bikes dynamics
+        at this current environment step
+        """
+        mask = (self.all_trips_data["Day"] == day) & (
+            self.all_trips_data["Month"] == month
+        )
+        current_trips = self.all_trips_data[mask]
+
+        # Keep only the trip at times we care:
+        timeshift = self.get_timeshift(time_counter)
+        start_time = timeshift[0]
+        end_time = timeshift[1]
+        time_mask = [
+            float(time.replace(":", ".").split(".")[0]) >= start_time
+            and float(time.replace(":", ".").split(".")[0]) <= end_time
+            for time in current_trips["StartTime"].values
+        ]
+        current_trips = current_trips[time_mask]
+
+        return current_trips
+
+    def get_centroids_demand(self, bikes_distribution, current_trips):
+        """
+        Compute the bikes needed for each centroid to complete as many trips
+        as possible
+
+        :param bikes_distribution: _description_
+        :param current_trips: _description_
+        """
+
+        demand = -np.array(bikes_distribution).copy()
+        for i in range(len(current_trips)):
+            trip = current_trips.iloc[i]
+            start_loc = np.array([trip.StartLatitude, trip.StartLongitude])
+            end_loc = np.array([trip.EndLatitude, trip.EndLongitude])
+            starting_distances = distance.cdist(
+                start_loc.reshape(-1, 2), self.centroids, metric="euclidean"
+            )
+            starting_centroid_idx_argsort = np.argsort(starting_distances)[0]
+            ending_distances = distance.cdist(
+                end_loc.reshape(-1, 2), self.centroids, metric="euclidean"
+            )
+            idx_end_centroid = np.argmin(ending_distances)
+
+            for j, idx_start_centroid in enumerate(starting_centroid_idx_argsort):
+                total_walking_distance = geopy.distance.distance(
+                    start_loc, self.centroids[idx_start_centroid, :]
+                ).km
+                if total_walking_distance > self.start_walk_dist_max:
+                    break
+                else:
+                    geo_ending_dist = geopy.distance.distance(
+                        end_loc, self.centroids[idx_end_centroid, :]
+                    ).km
+                    if geo_ending_dist <= self.end_walk_dist_max:
+                        demand[idx_start_centroid] += 1
+        return demand
+
+    def act(self, obs: np.ndarray, **_kwargs) -> np.ndarray:
+
+        month = int(obs[self.map_obs["month"]][0])
+        day = int(obs[self.map_obs["day"]][0])
+        time_counter = int(obs[self.map_obs["time_counter"]][0])
+        bikes_distribution = obs[self.map_obs["bikes_dist_after_shift"]]
+
+        current_trips = self.get_current_trips_data(
+            month=month, day=day, time_counter=time_counter
+        )
+        demand = self.get_centroids_demand(
+            bikes_distribution=bikes_distribution, current_trips=current_trips
+        )
+        best_centroids_idx = np.argpartition(demand, -self.num_trucks)[
+            -self.num_trucks :
+        ]
+
+        action = np.zeros(self.action_shape)
+        action[self.map_act["truck_centroid"]] = best_centroids_idx
+        action[self.map_act["truck_num_bikes"]] = self.bikes_per_truck
+
+        return action
+
+
+class CEMAgent(Agent):
+    def __init__(self) -> None:
+        super().__init__()
