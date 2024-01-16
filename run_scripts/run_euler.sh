9f29946d33f71b25e09182f3dbfde8683a20e18d
theauv <56826362+theauv@users.noreply.github.com>
2024-01-16 17:54:56 +0100

    managing

--- run_scripts/run_euler.sh
+++ run_scripts/run_euler.sh
@@ -12,21 +12,23 @@ pip install -e .
 
 # done
 
+
 group_name="euler_bikes_test"
-station_dependencies="src/env/bikes_data/factors_radius_1.npy"
-layers=3
-hid_size=400
+# station_dependencies="src/env/bikes_data/factors_radius_1.npy"
+# layers=3
+# hid_size=400
+# run_name="factored_${station_dependencies}_layers_${layers}_hidsize_${hid_size}"
 
-sbatch -n 1 --cpus-per-task=2 --time=24:00:00 --mem-per-cpu=1024 --output="output/%J" --mail-type=END --wrap="python3 run_scripts/train.py overrides=pets_bikes overrides.env_config.station_dependencies=${station_dependencies} dynamics_model=factored_simple dynamics_model.model.num_layers=${layers} dynamics_model.model.hid_size=${hid_size} experiment.run_configs.group=${group_name}"
+# sbatch -n 1 --cpus-per-task=2 --time=24:00:00 --mem-per-cpu=1024 --output="output/%J" --mail-type=END --wrap="python3 run_scripts/train.py overrides=pets_bikes overrides.env_config.station_dependencies=${station_dependencies} dynamics_model=factored_simple dynamics_model.model.num_layers=${layers} dynamics_model.model.hid_size=${hid_size} experiment.run_configs.group=${group_name} experiment.run_configs.name=${run_name}"
 
-# for station_dependencies in {1..5}
-#     do
-#     for layers in 2 4 6
-#         do
-#         for hid_size in 100 200 400
-#             do
-#             command="python3 run_scripts/train.py overrides=pets_bikes overrides.env_config.station_dependencies=${station_dependencies} dynamics_model=factored_simple dynamics_model.model.num_layers=${layers} dynamics_model.model.hid_size=${hid_size} experiment.run_configs.group=${group_name}"
-#             sbatch -n 1 --cpus-per-task=2 --time=24:00:00 --mem-per-cpu=1024 --output="output/%J" --mail-type=END --wrap=$command
-#         done
-#     done
-# done
+for station_dependencies in {1..5}
+    do
+    for layers in 2 4 6
+        do
+        for hid_size in 100 200 400
+            do
+            run_name="factored_${station_dependencies}_layers_${layers}_hidsize_${hid_size}"
+            sbatch -n 1 --cpus-per-task=2 --time=24:00:00 --mem-per-cpu=1024 --output="output/%J" --mail-type=END --wrap="python3 run_scripts/train.py overrides=pets_bikes overrides.env_config.station_dependencies=${station_dependencies} dynamics_model=factored_simple dynamics_model.model.num_layers=${layers} dynamics_model.model.hid_size=${hid_size} experiment.run_configs.group=${group_name} experiment.run_configs.name=${run_name}"
+        done
+    done
+done
