d9985f50f81cdc4de4e0868325c08b49c426b594
theauv <56826362+theauv@users.noreply.github.com>
2024-02-03 11:32:23 +0100

    euler benchmark

---
+++ run_scripts/bash_scripts/euler_benchmark.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+module load eth_proxy
+module load gcc/8.2.0 python/3.9.9
+source euler-pdm-env/bin/activate
+pip install -e .
+
+sbatch -n 1 --cpus-per-task=2 --time=24:00:00 --mem-per-cpu=1024 --output="output/%J" --wrap="python3 run_scripts/benchmark_run.py with_tracking=true agent='random' additional_run_name='1step'"
+sbatch -n 1 --cpus-per-task=2 --time=24:00:00 --mem-per-cpu=1024 --output="output/%J" --wrap="python3 run_scripts/benchmark_run.py with_tracking=true agent='good_heuristic' additional_run_name='1step'"
