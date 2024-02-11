14429fa0e0cb397e6f42a58ad0c9f1502f2f180d
theauv <56826362+theauv@users.noreply.github.com>
2024-02-11 18:15:14 +0100

    euler test target is delta

--- run_scripts/bash_scripts/run_euler.sh
+++ run_scripts/bash_scripts/run_euler.sh
@@ -5,22 +5,38 @@ module load gcc/8.2.0 python/3.9.9
 source euler-pdm-env/bin/activate
 pip install -e .
 
-
-group_name='artificial_4step_5centroid_test'
+group_name='test_targetisdelta_art_4step_5centroid'
 overrides='pets_bikes_5centroid'
 sbatch -n 1 --cpus-per-task=2 --time=24:00:00 --mem-per-cpu=1024 --output="output/%J" --wrap="python3 run_scripts/benchmark_run.py with_tracking=true overrides=${overrides} agent='random' group_name=${group_name}"
 sbatch -n 1 --cpus-per-task=2 --time=24:00:00 --mem-per-cpu=1024 --output="output/%J" --wrap="python3 run_scripts/benchmark_run.py with_tracking=true overrides=${overrides} agent='good_heuristic' group_name=${group_name}"
-for rescale_input in true false
+rescale_input=true
+for i in 1 ... 5
     do
     for rescale_output in true false
         do
         for target_is_delta in true false
             do
-            run_name="rescalein_${rescale_input}_rescaleout_${rescale_input}_targetdelta_${target_is_delta}"
+            run_name="rescalein_${rescale_input}_rescaleout_${rescale_output}_targetdelta_${target_is_delta}"
             sbatch -n 1 --cpus-per-task=2 --time=24:00:00 --mem-per-cpu=1024 --output="output/%J" --wrap="python3 run_scripts/train.py experiment.with_tracking=true overrides=${overrides} algorithm.rescale_input=${rescale_input} algorithm.rescale_output=${rescale_output} algorithm.target_is_delta=${target_is_delta} experiment.run_configs.name=${run_name} experiment.run_configs.group=${group_name}"
         done
     done
 done
+
+# group_name='artificial_4step_5centroid_test'
+# overrides='pets_bikes_5centroid'
+# sbatch -n 1 --cpus-per-task=2 --time=24:00:00 --mem-per-cpu=1024 --output="output/%J" --wrap="python3 run_scripts/benchmark_run.py with_tracking=true overrides=${overrides} agent='random' group_name=${group_name}"
+# sbatch -n 1 --cpus-per-task=2 --time=24:00:00 --mem-per-cpu=1024 --output="output/%J" --wrap="python3 run_scripts/benchmark_run.py with_tracking=true overrides=${overrides} agent='good_heuristic' group_name=${group_name}"
+# for rescale_input in true false
+#     do
+#     for rescale_output in true false
+#         do
+#         for target_is_delta in true false
+#             do
+#             run_name="rescalein_${rescale_input}_rescaleout_${rescale_output}_targetdelta_${target_is_delta}"
+#             sbatch -n 1 --cpus-per-task=2 --time=24:00:00 --mem-per-cpu=1024 --output="output/%J" --wrap="python3 run_scripts/train.py experiment.with_tracking=true overrides=${overrides} algorithm.rescale_input=${rescale_input} algorithm.rescale_output=${rescale_output} algorithm.target_is_delta=${target_is_delta} experiment.run_configs.name=${run_name} experiment.run_configs.group=${group_name}"
+#         done
+#     done
+# done
 
 # group_name='artificial_4step_20centroid'
 # overrides='pets_bikes_20centroid'
