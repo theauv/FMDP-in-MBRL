fixed sample/scaling

run_scripts/eval.py
@@ -208,12 +208,13 @@ class AdaptedVisualizer(Visualizer):
 
         if isinstance(self.dynamics_model.model, MultiOutputGP):
             for batch in self.dataset_train:
-                obs, act, next_obs, rewards, _, _ = batch.astuple()
-                train_x = torch.cat([torch.tensor(obs, dtype=torch.float32), torch.tensor(act, dtype=torch.float32)], axis=-1)
-                target = torch.tensor(next_obs, dtype=torch.float32)
-                self.dynamics_model.model.train()
-                self.dynamics_model.model.set_train_data(train_x, target)
-                self.dynamics_model.model.forward()
+                self.dynamics_model.loss(batch)
+                # obs, act, next_obs, rewards, _, _ = batch.astuple()
+                # train_x = torch.cat([torch.tensor(obs, dtype=torch.float32), torch.tensor(act, dtype=torch.float32)], axis=-1)
+                # target = torch.tensor(next_obs, dtype=torch.float32)
+                # self.dynamics_model.model.train()
+                # self.dynamics_model.model.set_train_data(train_x, target)
+                # self.dynamics_model.model.forward()
 
         # Instanciate the agent
         self.agent: mbrl.planning.Agent
@@ -227,7 +228,7 @@ class AdaptedVisualizer(Visualizer):
                 == "mbrl.planning.TrajectoryOptimizerAgent"
             ):
                 print("Agent uses TrajectoryOptimizer")
-                agent_cfg.algorithm.agent.planning_horizon = lookahead
+                #agent_cfg.algorithm.agent.planning_horizon = lookahead
                 self.agent = mbrl.planning.create_trajectory_optim_agent_for_model(
                     self.model_env,
                     agent_cfg.algorithm.agent,
@@ -269,6 +270,7 @@ class AdaptedVisualizer(Visualizer):
                 observation
             )  # this is where you would insert your policy
             observation, reward, terminated, truncated, info = self.env.step(action)
+            print(f"Step Reward: {reward}")
 
             n_steps += 1
             rewards += reward
@@ -280,6 +282,7 @@ class AdaptedVisualizer(Visualizer):
                 observation, info = self.env.reset()
                 n_steps = 0
                 rewards = 0
+                print(f"Episode Reward: {rewards}")
         self.env.close()
 
         print(f"Mean reward: {np.mean(all_rewards)}")
@@ -302,10 +305,6 @@ class AdaptedVisualizer(Visualizer):
 
         # create env and random seed
         observation, info = self.env.reset(seed=42)
-        model_observation = {
-            "obs": np.expand_dims(observation, axis=0),
-            "propagation_indices": None,
-        }
 
         real_trajectories = []
         model_trajectories = []
@@ -318,34 +317,64 @@ class AdaptedVisualizer(Visualizer):
             print("--------------------- \n" f"Env step: {env_step}")
 
             # Env step
+            from time import time
+            start = time()
             action = self.agent.act(observation)
+            model_observation = {
+                "obs": np.expand_dims(observation.copy(), axis=0),
+                "propagation_indices": None,
+            }
+            model_action = np.expand_dims(action.copy(), axis=0)
+            print("Act time: ", time()-start)
             new_obs, reward, terminated, truncated, info = self.env.step(action)
             real_trajectories.append([observation, action, new_obs])
             print("Real env")
-            print(f"Obs: {observation}, Action: {action}, New_obs: {new_obs}")
+            print(
+                f"Obs: {observation} \n"
+                f"Action: {action}\n"
+                f"New_obs: {new_obs}\n"
+                f"Reward: {reward}"
+            )
+            if hasattr(self.env.unwrapped, "delta_bikes"):
+                print(
+                    f"Old bikes distr: {observation[self.env.get_wrapper_attr('map_obs')['bikes_distr']]}"
+                    f"Delta_bikes: {self.env.get_wrapper_attr('delta_bikes')} \n"
+                    f"Pre-new_bikes distr: {self.env.get_wrapper_attr('previous_bikes_distr')} \n"
+                    f"New bikes distr: {new_obs[self.env.get_wrapper_attr('map_obs')['bikes_distr']]}"
+                )
+
             observation = new_obs
 
-            # Model_env step
-            # if env_step == 1 or np.all(model_observation["obs"][0] == observation):
-            #     model_action = action
-            # else:
-            #     model_action = self.agent.act(model_observation["obs"][0])
-            # model_action = np.expand_dims(model_action, axis=0)
-            model_action = np.expand_dims(action, axis=0)
-
-            old_model_observation = model_observation["obs"][0].copy()
-
-            _, _, _, model_observation = self.model_env.step(
-                model_action, model_observation
+            model_observation_ = {
+                "obs": model_observation["obs"].copy(),
+                "propagation_indices": None,
+            }
+            model_action_ = model_action.copy()
+            next_observs, rewards, dones, new_model_observation = self.model_env.step(
+                model_action_, model_observation_
             )
-            model_observation["obs"] = model_observation["obs"].detach().numpy()
+            model_observation = model_observation["obs"][0]
+            model_observation_ = model_observation_["obs"][0].detach().numpy()
+            new_model_observation = new_model_observation["obs"][0]
+            next_observs = next_observs.detach().numpy()[0]
+            rewards = rewards.detach().numpy()
             model_trajectories.append(
-                [old_model_observation, model_action[0], model_observation["obs"][0]]
+                [model_observation, model_action, new_model_observation]
             )
             print("Model env")
             print(
-                f"Obs: {old_model_observation}, Action: {model_action}, New_obs: {model_observation['obs'][0]}"
+                f"Obs: {model_observation} \n"
+                f"Action: {model_action} \n"
+                f"New_obs: {next_observs}\n" 
+                f"Rewards: {rewards}"
             )
+            if hasattr(self.env.unwrapped, "delta_bikes"):
+                print(
+                    f"Old bikes distr: {model_observation[self.env.get_wrapper_attr('map_obs')['bikes_distr']]}"
+                    f"Delta_bikes: {model_observation_[self.env.get_wrapper_attr('map_obs')['bikes_distr']] - model_observation[self.env.get_wrapper_attr('map_obs')['bikes_distr']]} \n"
+                    f"Pre-new_bikes distr: {model_observation_[self.env.get_wrapper_attr('map_obs')['bikes_distr']]} \n"
+                    f"New bikes distr: {next_observs[self.env.get_wrapper_attr('map_obs')['bikes_distr']]}"
+                )
 
         env_states = [traj[0] for traj in real_trajectories]
         env_states.append(real_trajectories[-1][-1])

src/model/dict_model_wrapper.py
@@ -39,6 +39,7 @@ class OneDTransitionRewardModelDictSpace(OneDTransitionRewardModel):
         target_is_delta: bool = True,
         normalize: bool = False,
         rescale_input: bool = False,
+        rescale_output: bool = False,
         normalize_double_precision: bool = False,
         learned_rewards: bool = True,
         obs_preprocess_fn: Optional[mbrl.types.ObsProcessFnType] = None,
@@ -102,30 +103,33 @@ class OneDTransitionRewardModelDictSpace(OneDTransitionRewardModel):
         if self.learned_rewards:
             model_output_length += 1
 
-        # TODO: remove this line
-        self.output_normalizer = None
+        #Rescaling/Normalization
         self.rescale_input = rescale_input
-        if self.rescale_input:
+        self.rescale_output = rescale_output
+        if self.rescale_input or self.rescale_output:
             self.rescale_obs = rescale_obs
             self.rescale_act = rescale_act
         if normalize:
-            self.output_normalizer = mbrl.util.math.Normalizer(
-                model_output_length,
-                self.model.device,
-                dtype=torch.double if normalize_double_precision else torch.float,
-            )
+            #TODO: (re)-add a posssibility to nromalize input/output instead of rescale
+            self.input_normalizer = None #Initialized in mother class
+            warnings.warn(
+                "The normalizer is disabled for now as we only want to use "
+                "a rescaling process."
+                )
 
         print("--------------MODEL INFO----------------")    
         print(f"Model obs input keys: {model_input_obs_key}")
         print(f"Model action input keys: {model_input_act_key}")
         print(f"Model output keys: {model_output_key}")
-        print(f"Normalize output: {normalize}")
         print(f"Rescale input: {rescale_input}")
+        print(f"Rescale output: {rescale_output}")
         print(f"Target is delta: {target_is_delta}")
         print(f"Learne rewards: {learned_rewards}")
         print(f"Num elites: {num_elites}")
 
     def _get_next_obs(self, batch_next_obs: mbrl.types.TensorType):
+        if self.rescale_output:
+            batch_next_obs = self.rescale_obs(batch_next_obs)
         if len(batch_next_obs.shape) == 1:
             batch_next_obs = np.expand_dims(batch_next_obs, axis=0)
         if not np.any(self.model_output_mask):
@@ -138,12 +142,15 @@ class OneDTransitionRewardModelDictSpace(OneDTransitionRewardModel):
     ) -> torch.Tensor:
         if self.obs_process_fn:
             obs = self.obs_process_fn(obs, action)
+        
         obs = model_util.to_tensor(obs).to(self.device)
         action = model_util.to_tensor(action).to(self.device)
         if self.rescale_input:
-            obs = self.rescale_obs(obs)
-            action = self.rescale_act(action)
-        model_in = torch.cat([obs, action], dim=obs.ndim - 1)
+            obs_ = self.rescale_obs(obs.clone())
+            action_ = self.rescale_act(action.clone())
+            model_in = torch.cat([obs_, action_], dim=obs.ndim - 1)
+        else:
+            model_in = torch.cat([obs, action], dim=obs.ndim - 1)
 
         model_in = model_in.float().to(self.device)
         masked_model_in = model_in[..., self.model_input_mask]
@@ -164,6 +171,7 @@ class OneDTransitionRewardModelDictSpace(OneDTransitionRewardModel):
             target_obs = model_util.to_tensor(target_obs).to(self.device)
 
         model_in, _ = self._get_model_input(obs, action)
+
         if self.learned_rewards:
             reward = model_util.to_tensor(reward).to(self.device).unsqueeze(reward.ndim)
             if target_obs is not None:
@@ -176,28 +184,23 @@ class OneDTransitionRewardModelDictSpace(OneDTransitionRewardModel):
         if target is None:
             raise ValueError("You try to predict nothing")
 
-        if self.output_normalizer:
-            target = self.output_normalizer.normalize(target)
-
         return model_in.float(), target.float()
 
     def forward(self, x: torch.Tensor, *args, **kwargs) -> Tuple[torch.Tensor, ...]:
         """Calls forward method of base model with the given input and args."""
-
-        warnings.warn("Not used so far, make sure it works")
-
-        if len(x.shape) == 1:
-            x = x[None, ...]
-
-        obs = x[: self.obs_length]
-        action = x[self.obs_length : self.obs_length + self.act_length]
-        model_input = self._get_model_input(obs, action)
-        output = obs
-        sub_output = self.model.forward(model_input, *args, **kwargs).numpy()
-        output[self.model_output_mask] = sub_output
-        output = self.obs_postprocess_fn(output)
-        # TODO: Denormalize output ???
-        return output
+        # TODO: badly implemented, but not used anyway, must be (re)-implemented
+        # if len(x.shape) == 1:
+        #     x = x[None, ...]
+        # obs = x[: self.obs_length]
+        # action = x[self.obs_length : self.obs_length + self.act_length]
+        # model_input = self._get_model_input(obs, action)
+        # output = obs
+        # sub_output = self.model.forward(model_input, *args, **kwargs).numpy()
+        # output[self.model_output_mask] = sub_output
+        # output = self.obs_postprocess_fn(output)
+        # # TODO: Denormalize output ???
+        # return output
+        raise NotImplementedError
 
     def update_normalizer(self, batch: mbrl.types.TransitionBatch):
         """Updates the normalizer statistics using the batch of transition data.
@@ -209,28 +212,10 @@ class OneDTransitionRewardModelDictSpace(OneDTransitionRewardModel):
             batch (:class:`mbrl.types.TransitionBatch`): The batch of transition data.
                 Only next_obs and reward will be used, since these are the outputs to the model.
         """
-        if self.output_normalizer is None:
+        #TODO: (Re)-implement the normalizer for modularize code (not necessary for now)
+        if self.input_normalizer is None:
             return
-        obs, next_obs, reward = batch.obs, batch.next_obs, batch.rewards
-        if self.target_is_delta:
-            target_obs = next_obs - obs
-            for dim in self.no_delta_list:
-                target_obs[..., dim] = next_obs[..., dim]
-        else:
-            target_obs = next_obs
-        target_obs = self._get_next_obs(target_obs)
-        if reward.ndim == 1:
-            reward = np.expand_dims(reward, axis=-1)
-        if target_obs is None:
-            target = reward
-        else:
-            if target_obs.ndim == 1:
-                target_obs = np.expand_dims(target_obs, axis=-1)
-            if self.learned_rewards:
-                target = np.concatenate([target_obs, reward], axis=obs.ndim - 1)
-            else:
-                target = target_obs
-        self.output_normalizer.update_stats(target)
+        raise NotImplementedError
 
     def get_output_and_targets(
         self, batch: mbrl.types.TransitionBatch
@@ -247,18 +232,17 @@ class OneDTransitionRewardModelDictSpace(OneDTransitionRewardModel):
         Returns:
             (tuple(tensor), tensor): the model outputs and the target for this batch.
         """
-        warnings.warn("Not used so far, make sure it works")
-        with torch.no_grad():
-            model_in, target = self._process_batch(batch)
-            sub_output = self.model.forward(model_in)
-            if self.output_normalizer:
-                sub_output = self.output_normalizer.denormalize(sub_output)
-
-            out = model_in
-            out[..., self.model_output_mask] = sub_output
-            out = self.obs_postprocess_fn(out)
-
-            return out, target
+        # TODO: badly implemented, but not used anyway, must be (re)-implemented
+        # with torch.no_grad():
+        #     model_in, target = self._process_batch(batch)
+        #     sub_output = self.model.forward(model_in)
+        #     if self.output_normalizer:
+        #         sub_output = self.output_normalizer.denormalize(sub_output)
+        #     out = model_in
+        #     out[..., self.model_output_mask] = sub_output
+        #     out = self.obs_postprocess_fn(out)
+        #     return out, target
+        raise NotImplementedError
 
     def sample(
         self,
@@ -277,6 +261,14 @@ class OneDTransitionRewardModelDictSpace(OneDTransitionRewardModel):
         This wrapper assumes that the underlying model's sample method returns a tuple
         with just one tensor, which concatenates next_observation and reward.
 
+        We want to sample (next_obs, reward) from (obs, act) using the following function:
+        (obs, act) -preprocess-> (new_obs, act) -model(.)-> (new_obs, preds) -postprocess-> (new_obs, preds)
+        This way, one can predict only a subspace of the full observation space and call pre/post processed
+        function that are environment-dependent to deal with the rest. We can also of course just predict
+        the full next_obs. But it is typically useful in a setting like the Bikes environment in which the 
+        addition of bikes at each timestep and the incrementation of the time coutner is known. But only the
+        bikes distribution needs to be learned.
+
         Args:
             act (tensor): the action at.
             model_state (tensor): the model state st.
@@ -287,7 +279,6 @@ class OneDTransitionRewardModelDictSpace(OneDTransitionRewardModel):
         Returns:
             (tuple of two tensors): predicted next_observation (o_{t+1}) and rewards (r_{t+1}).
         """
-        obs = model_util.to_tensor(model_state["obs"]).to(self.device)
         model_in, preprocessed_obs = self._get_model_input(model_state["obs"], act)
         if not hasattr(self.model, "sample_1d"):
             raise RuntimeError(
@@ -296,20 +287,24 @@ class OneDTransitionRewardModelDictSpace(OneDTransitionRewardModel):
         preds, next_model_state = self.model.sample_1d(
             model_in, model_state, rng=rng, deterministic=deterministic
         )
-        if self.output_normalizer is not None:
-            preds = self.output_normalizer.denormalize(preds).float()
         next_observs = preds[:, :-1] if self.learned_rewards else preds
+        if self.rescale_output:
+            tmp=torch.zeros(preprocessed_obs.shape)
+            tmp[:, self.model_output_mask] = next_observs
+            tmp = self.rescale_obs(tmp, scale=True)
+            next_observs = tmp[:, self.model_output_mask]
 
         next_obs = preprocessed_obs
-        if next_observs.shape[-1] > 0:
-            next_obs[:, self.model_output_mask] = next_observs
+        if next_observs.shape[-1] > 0:           
+            if self.target_is_delta:
+                next_obs[:, self.model_output_mask] += next_observs
+                for dim in self.no_delta_list:
+                    next_obs[:, dim] = next_observs[:, dim]
+            else:
+                next_obs[:, self.model_output_mask] = next_observs
+
         next_obs = self.obs_postprocess_fn(next_obs)
 
-        if self.target_is_delta:
-            tmp_ = next_obs + obs
-            for dim in self.no_delta_list:
-                tmp_[:, dim] = next_obs[:, dim]
-            next_obs = tmp_
         rewards = preds[:, -1:] if self.learned_rewards else None
         next_model_state["obs"] = next_obs
 

src/model/gaussian_process.py
@@ -114,7 +114,11 @@ class MultiOutputGP(Model):
         if x is None:
             return self.gp(*self.gp.train_inputs)
         else:
-            return [model(x) for model in self.gp.models]
+            from time import time
+            start = time()
+            x = x.unsqueeze(0).repeat(self.out_size, 1, 1)
+            loss = self.gp(*x)
+            return loss
 
     def loss(self, model_in: torch.Tensor, target: torch.Tensor = None) -> torch.Tensor:
         # TODO: might be avoidably very computationally expensive !!
@@ -135,8 +139,8 @@ class MultiOutputGP(Model):
     def pred_distribution(
         self, model_in: torch.Tensor
     ) -> torch.distributions.Distribution:
+        self.eval()
         with torch.no_grad(), gpytorch.settings.fast_pred_var():
-            self.eval()  # Here or outside ??
             return self.likelihood(*self.forward(model_in))
 
     def eval_score(
@@ -146,15 +150,14 @@ class MultiOutputGP(Model):
     ) -> Tuple[torch.Tensor, Dict[str, Any]]:
         # if self.models.train_inputs ar all None -> None
         assert model_in.ndim == 2 and target.ndim == 2
-        with torch.no_grad(), gpytorch.settings.fast_pred_var():
-            pred_output = self.pred_distribution(model_in)
-            pred_mean = torch.cat(
-                [pred.mean.unsqueeze(-1) for pred in pred_output], axis=-1
-            )
-            meta = {
-                "outputs": pred_mean,
-                "targets": target,
-            }
+        pred_output = self.pred_distribution(model_in)
+        pred_mean = torch.cat(
+            [pred.mean.unsqueeze(-1) for pred in pred_output], axis=-1
+        )
+        meta = {
+            "outputs": pred_mean,
+            "targets": target,
+        }
         return F.mse_loss(pred_mean, target, reduction="none").unsqueeze(0), meta
 
     def save(self, save_dir: Union[str, pathlib.Path]):
@@ -234,9 +237,7 @@ class FactoredMultiOutputGP(MultiOutputGP):
                     model_factors=self.reward_model_factors,
                 )
 
-        print(self.factors)
         self.model_factors = self.get_model_factors(self.factors)
-        print(self.model_factors)
         self.models = self.get_factored_models()
 
         self.gp = gpytorch.models.IndependentModelList(*self.models)