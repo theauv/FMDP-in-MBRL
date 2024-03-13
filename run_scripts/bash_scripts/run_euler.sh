060c4aa1d8046a23f0f328de9f5e3f9cc3f30e8b
theauv <56826362+theauv@users.noreply.github.com>
2024-03-13 11:47:23 +0100

    euler bikes

--- run_scripts/bash_scripts/run_euler.sh
+++ run_scripts/bash_scripts/run_euler.sh
@@ -7,6 +7,7 @@
 # pip install -e .
 
 group_name='real_4step'
+#group_name='art_4step'
 overrides='pets_bikes'
 #rescale_input=true
 rescale_output=true
@@ -31,7 +32,8 @@ for i in {1..3}
 done
 
 group_name='real_4step_20centroid'
-overrides='pets_bikes_20_centroid'
+#group_name='art_4step_20centroid'
+overrides='pets_bikes_20centroid'
 trips_data="src/env/bikes_data/all_trips_LouVelo_merged.csv"
 weather_data="src/env/bikes_data/weather_data.csv"
 initial_exploration_steps=50
@@ -44,6 +46,7 @@ for i in {1..3}
         for rescale_output in true false
             do
             run_name="${model}_${obs_postprocess_fn}_targetdelta_${target_is_delta}_${i}"
+            #sbatch -n 1 --cpus-per-task=4 --time=48:00:00 --mem-per-cpu=4000 --output="output/%J" --wrap="python3 run_scripts/train.py experiment.with_tracking=true overrides=${overrides} overrides.obs_postprocess_fn=${obs_postprocess_fn} overrides.initial_exploration_steps=${initial_exploration_steps} dynamics_model=${model} algorithm.rescale_input=${rescale_input} algorithm.rescale_output=${rescale_output} algorithm.target_is_delta=${target_is_delta} experiment.run_configs.name=${run_name} experiment.run_configs.group=${group_name}"
             sbatch -n 1 --cpus-per-task=4 --time=48:00:00 --mem-per-cpu=4000 --output="output/%J" --wrap="python3 run_scripts/train.py experiment.with_tracking=true overrides=${overrides} overrides.obs_postprocess_fn=${obs_postprocess_fn} overrides.initial_exploration_steps=${initial_exploration_steps} overrides.env_config.past_trip_data=${trips_data} overrides.env_config.weather_data=${weather_data} dynamics_model=${model} algorithm.rescale_input=${rescale_input} algorithm.rescale_output=${rescale_output} algorithm.target_is_delta=${target_is_delta} experiment.run_configs.name=${run_name} experiment.run_configs.group=${group_name}"
         done
     done
